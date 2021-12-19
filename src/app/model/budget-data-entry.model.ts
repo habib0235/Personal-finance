5aecbf95461dee46b003dc234020da41ff0cfa7d
Habib ALHABIB <habib.alhabib@esprit.tn>
2021-12-19 23:42:14 +0100

    ht-11 Create data structure to be used in editable table
    Added basic data structure

---
+++ src/app/model/budget-data-entry.model.ts
@@ -0,0 +1,16 @@
+export interface BudgetDataEntry {
+
+    label: string;
+    jan: number;
+    feb: number;
+    mar: number;
+    apr: number;
+    may: number;
+    jun: number;
+    jul: number;
+    aug: number;
+    sep: number;
+    oct: number;
+    nov: number;
+    dec: number;
+}
