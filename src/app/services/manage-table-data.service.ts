39080005eb40610b52ad5d2348feeee400c105a1
Habib ALHABIB <habib.alhabib@esprit.tn>
2021-12-22 16:25:06 +0100

    ht-10 Add service class to editable table
    Added service class to editable table

---
+++ src/app/services/manage-table-data.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManageTableDataService {
+
+  constructor() { }
+
+
+  public add(): void {
+    // Add budget entry
+  }
+
+  public update(): void {
+    // Update budget entry
+  }
+
+  public delete(): void {
+    // Delete budget entry
+  }
+
+}
